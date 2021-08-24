628adb6593cb717f7f967838ba92eabbdb18e8ab
Alperen <devcalprn@gmail.com>
2021-08-24 19:01:08 +0300

    Constants.kt added

---
+++ app/src/main/java/com/cevlikalprn/safeargswithnavigation/Constants.kt
@@ -0,0 +1,10 @@
+package com.cevlikalprn.safeargswithnavigation
+
+object Constants {
+    const val avatar1 = R.drawable.avatar1
+    const val avatar2 = R.drawable.avatar2
+    const val avatar3 = R.drawable.avatar3
+    const val avatar4 = R.drawable.avatar4
+    const val avatar5 = R.drawable.avatar5
+    const val avatar6 = R.drawable.avatar6
+}
